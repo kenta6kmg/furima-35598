6d7423e30b7a9325a79616065ec3e57bd13b5a91
kenta6kmg <atk_53.v@yahoo.ne.jp>
2021-05-18 11:44:22 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :buys
+
+  validates :nickname,           presence: true
+  validates :email,              presence: true
+  validates :encrypted_password, presence: true
+  validates :last_name,          presence: true
+  validates :first_name,         presence: true
+  validates :ruby_last,          presence: true
+  validates :ruby_first,         presence: true
+  validates :birth,              presence: true
 end
