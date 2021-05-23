adc9bf4c5a211a79232a0ff4fa9c87709fea79b5
kenta6kmg <atk_53.v@yahoo.ne.jp>
2021-05-23 14:47:16 +0900

    商品出品機能（修正１）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -18,7 +18,7 @@ end
       expect(@item.errors.full_messages).to include("User must exist")
      end
     it 'titleが空では保存できない' do
-     @item.title = ''
+     @item.title = nil
      @item.valid?
      expect(@item.errors.full_messages).to include("Title can't be blank")
     end
@@ -28,37 +28,37 @@ end
       expect(@item.errors.full_messages).to include("Image can't be blank")
     end
     it 'item_descriptionが空では保存できない' do
-      @item.item_description  = ''
+      @item.item_description  = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Item description can't be blank")
     end
     it 'item_category_idがid1では保存できない' do
-      @item.item_category_id  = '1'
+      @item.item_category_id  = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Item category must be other than 1")
     end
     it 'item_condition_idがid1では保存できない' do
-      @item.item_condition_id  = '1'
+      @item.item_condition_id  = 1
       @item.valid?
     expect(@item.errors.full_messages).to include("Item condition must be other than 1")
    end
     it 'shipping_charge_idがid1では保存できない' do
-      @item.shipping_charge_id  = '1'
+      @item.shipping_charge_id  = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping charge must be other than 1")
     end
     it 'shipping_area_idがid1では保存できない' do
-      @item.shipping_area_id  = '1'
+      @item.shipping_area_id  = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
     end
     it 'day_idがid1では保存できない' do
-      @item.day_id  = '1'
+      @item.day_id  = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Day must be other than 1")
     end
     it 'priceが空では保存できない' do
-      @item.price = ''
+      @item.price = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Price can't be blank")
     end
@@ -68,14 +68,20 @@ end
       expect(@item.errors.full_messages).to include("Price is invalid")
     end
     it 'priceが300以下では保存できない' do
-      @item.price = '299'
+      @item.price = 299
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is invalid")
     end
     it 'priceが9999999以上ではでは保存できない' do
-      @item.price = '10000000'
+      @item.price = 10000000
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is invalid")
     end
+      it '英数字混合では保存できない' do
+        @item.price = 'a1'
+        @item.valid?
+        binding.pry
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+    end
   end
-end
