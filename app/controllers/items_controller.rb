456d5b5ac55bf71327abcbd6b2297a77563dba95
kenta6kmg <atk_53.v@yahoo.ne.jp>
2021-05-25 15:18:58 +0900

    商品削除機能（修正１）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :redirect_item, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :redirect_item, only: [:edit, :update, :destroy]
   
 
   def index
@@ -36,11 +36,8 @@ class ItemsController < ApplicationController
     end
 
     def destroy
-      item = Item.find(params[:id])
-      if current_user.id == item.user_id
-          item.destroy
+          @item.destroy
           redirect_to root_path
-      end
     end
 
 
