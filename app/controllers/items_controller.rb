7088184ace4ef3b8ef9c192862d658dfce4be84d
kenta6kmg <atk_53.v@yahoo.ne.jp>
2021-05-25 15:01:48 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,16 @@ class ItemsController < ApplicationController
         render :edit
       end
     end
-  
+
+    def destroy
+      item = Item.find(params[:id])
+      if current_user.id == item.user_id
+          item.destroy
+          redirect_to root_path
+      end
+    end
+
+
 
   private
   def item_params
