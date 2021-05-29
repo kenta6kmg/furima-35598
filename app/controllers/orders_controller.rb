9b56beadbcd130515ce6307937577de01ae05c41
kenta6kmg <atk_53.v@yahoo.ne.jp>
2021-05-29 11:49:18 +0900

    商品購入（修正４）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -43,6 +43,7 @@ class OrdersController < ApplicationController
          card: order_params[:token],    # カードトークン
          currency: 'jpy'                # 通貨の種類（日本円）
       )
+    end
   end
 end
 
