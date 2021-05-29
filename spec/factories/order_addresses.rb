bb70bf5a0e8ea9ad209c3fba3ef7a2c99d6ab24a
kenta6kmg <atk_53.v@yahoo.ne.jp>
2021-05-29 11:54:14 +0900

    商品購入機能（修正４）

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     shipping_area_id   {2}
     city               { '東京都' }
     house_number       { '1-1' }
+    building_name      {Faker::Name.initials}
     phone_number       { '00000000000' }
     token              {"tok_abcdefghijk00000000000000000"}
   end
